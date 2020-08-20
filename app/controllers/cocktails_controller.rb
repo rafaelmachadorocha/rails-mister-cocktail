34ba8cdfe1d4722a406d5bf38ab93fb088089744
rafaelrocha <rafaelmachadodarocha@gmail.com>
2020-08-20 15:11:04 -0300

    implmenting cocktails controller

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -4,14 +4,32 @@ class CocktailsController < ApplicationController
   end
 
   def show
+    @cocktail = Cocktail.find(params[:id])
   end
 
   def new
+    @cocktail = Cocktail.new
   end
 
   def create
+    @cocktail = Cocktail.new(set_params)
+    if @cocktail.valid?
+      @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def set_params
+    params.require(:cocktail).permit(:name)
   end
 end
